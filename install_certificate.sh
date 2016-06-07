5c2292430a010fa8d6b91698e12b31e158b55c62
Kenneth Verbeure <verbeurk@awingu.com>
2016-06-07 11:28:08 +0200

    Use bash script to also install certificates

--- install_certificate.sh
+++ install_certificate.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+DNS=$1
+DOMAIN=$2
+ADMINPASS="$3"
+USER="$4"
+PASS="$5"
+
 dpkg -i awingu-license-certificate_99999_all.deb
+python setup_awingu.py --dns $DNS --domain $DOMAIN --admin-pass "$ADMINPASS" --domain-admin "$USER" --domain-pass "$PASS"
