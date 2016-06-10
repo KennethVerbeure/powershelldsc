ea1f3716d0863631152fcf1a0b15b113a184304a
Kenneth Verbeure <verbeurk@awingu.com>
2016-06-10 14:33:17 +0200

    Fixes

--- install_certificate.sh
+++ install_certificate.sh
@@ -5,6 +5,7 @@ DOMAIN=$2
 ADMINPASS="$3"
 USER="$4"
 PASS="$5"
+ADMACHINENAME = "$6"
 
 dpkg -i awingu-license-certificate_99999_all.deb
-python setup_awingu.py --dns $DNS --domain $DOMAIN --admin-pass "$ADMINPASS" --domain-admin "$USER" --domain-pass "$PASS"
+python setup_awingu.py --dns $DNS --domain $DOMAIN --admin-pass "$ADMINPASS" --domain-admin "$USER" --domain-pass "$PASS" --ad-machine-name "$ADMACHINENAME"
